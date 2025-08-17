d8c78c069a14431eba9310939b8f0845a91b6515
Oliver von Olnhausen <info@olnhausendesign.se>
2025-08-17 18:41:18 +0200

    Add CampResource.php

--- app/Http/Resources/CampResource.php
+++ app/Http/Resources/CampResource.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Camp;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/** @mixin Camp */
 class CampResource extends JsonResource
 {
     public function toArray(Request $request): array

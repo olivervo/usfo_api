ede5dc6d9ffb0d6ed8d60e33d9af90cab160c12b
Oliver von Olnhausen <info@olnhausendesign.se>
2025-08-19 22:44:20 +0200

    Add permission check for viewing camps in `CampPolicy`

--- app/Policies/CampPolicy.php
+++ app/Policies/CampPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Enums\Permissions;
 use App\Models\Camp;
 use App\Models\User;
 
@@ -9,6 +10,10 @@ class CampPolicy
 {
     public function view(?User $user, Camp $camp): bool
     {
+        if ($user?->can(Permissions::CampsView->name)) {
+            return true;
+        }
+
         return $camp->isPublished();
     }
 }

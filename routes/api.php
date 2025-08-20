ce341597cc4b4604d9a895bb0ccfcc980dbb05af
Oliver von Olnhausen <info@olnhausendesign.se>
2025-08-20 14:05:35 +0200

    WIP

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\CampController;
+use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Public routes
+Route::get('/camps', [CampController::class, 'index']);
+Route::get('/camps/{camp}', [CampController::class, 'show']);
+Route::post('/registrations', [RegistrationController::class, 'store']);
+
+// Staff routes
+
+// Admin routes
+Route::apiResource('/registrations', RegistrationController::class);
+Route::apiResource('/students', StudentController::class);
